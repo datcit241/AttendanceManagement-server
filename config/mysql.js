d7835e9d3eaacf3aebc39ccf0bf02ffb7bb58a64
Dat Vo <dat.cit241@gmail.com>
2023-05-09 15:09:37 +0700

    Switch from MongoDB to MySQL

---
+++ config/mysql.js
@@ -0,0 +1,14 @@
+let mysql = require('mysql');
+let conn = mysql.createConnection({
+  host: 'localhost', // Replace with your host name
+  user: 'root',      // Replace with your database username
+  password: '1234',      // Replace with your database password
+  database: 'attendance_management' // // Replace with your database Name
+}); 
+ 
+conn.connect(function(err) {
+  if (err) throw err;
+  console.log('Database is connected successfully!');
+});
+
+module.exports = conn;
